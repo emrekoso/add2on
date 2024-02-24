f65c2eba948fd8f51726c8bbfa457a127e34695d
emre <emrecelikfw@gmail.com>
2024-02-24 13:58:45 +0300

    add room partners shown&&colors

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -33,8 +33,8 @@ export async function POST(request: Request) {
 
 
     const userInfo = {
-        name: user.firstName!,
-        picture: user.imageUrl!,
+        name: user.firstName || "Teammate",
+        picture: user.imageUrl,
     }
 
 
